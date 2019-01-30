00f1f5ff84a59d6bce2e193bf978998d26de0431
Anne Chepkeitany <rerimoianne.4@gmail.com>
2019-01-30 10:47:36 +0300

    Empty Screen added, a bare ChatWindow added as well

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import user from "./user";
 import contacts from "./contacts";
+import activeUserId from "./activeUserId";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
     user,
-    contacts
+    contacts,
+    activeUserId
 });
 
 export default rootReducer;
